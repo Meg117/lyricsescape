a554fe3faa7b30f0baf47957752bb738bf6d75cb
Meg117 <meghnachauhan0097@gmail.com>
2018-09-12 21:01:45 +0530

    first commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Navbar=() => {
+  return (
+    <nav className="navbar navbar-dark bg-secondary mb-5">
+    <span className="navbar-brand mb-0 h1 mx-auto">Escape</span>
+      
+    </nav>
+  )
+}
+export default Navbar;  
